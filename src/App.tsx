755817ca419ba10a08db24ee756ad32e8aa33eb6
QZ <3240973795@qq.com>
2025-01-07 01:35:00 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>click me</button>
+    </>
+  );
+}
+
+export default App;
