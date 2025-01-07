b5bea1d19178c9dcab6839541a87ab852febabd6
QZ <3240973795@qq.com>
2025-01-08 00:51:36 +0800

    init: less

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "@/App";
-import 'normalize.css'
+import "normalize.css";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>
 );
---
+++ src/router/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { RouteObject } from "react-router-dom"
+
+const routes: RouteObject[] = [
+    {
+        path: '/',
+        element: <div/>,
+    }
+]
+
+export default routes
